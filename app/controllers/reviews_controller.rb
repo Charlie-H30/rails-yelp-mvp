47dcb2227800b519df69012c9a5a6a72e00b036d
Charlie Hartigan <charliehartigan@icloud.com>
2025-02-26 15:22:12 +0000

    YELP MVP

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,25 @@
+class ReviewsController < ApplicationController
+  before_action :set_restaurant, only: [:new, :create]
+  # def new
+  #   @review = Review.new
+  # end
+
+  def create
+    @review = @restaurant.reviews.new(review_params)
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,25 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Cleaning database ..."
+Review.destroy_all
+Restaurant.destroy_all
+
+puts "Creating instances"
+Restaurant.create!(name: "Nandos", address: "Birmingham", phone_number: "999", category: "chinese")
+puts "Created Nandos"
+
+Restaurant.create!(name: "Gary", address: "The Ocean", phone_number: "+1145", category: "italian")
+puts "Created Gary"
+
+Restaurant.create!(name: "Baguette City", address: "France", phone_number: "111", category: "french")
+puts "Created BC"
+
+Restaurant.create!(name: "Sushi Plaza", address: "45 Long Island", phone_number: "505", category: "japanese")
+puts "Created SP"
+
+Restaurant.create!(name: "Maxime", address: "Quarry", phone_number: "7772", category: "belgian")
+puts "Created Maxime"
+
+puts "Finished! Created #{Restaurant.count} restaurants"
